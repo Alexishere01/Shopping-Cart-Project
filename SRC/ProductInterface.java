4d9b3b1a5541620d71e7f19bb440583a024edd0a
Austin Harrison <austin.harrisonn@yahoo.com>
2024-11-30 23:13:37 -0500

    inital commit test

--- SRC/ProductInterface.java
+++ SRC/ProductInterface.java
@@ -1,11 +1,11 @@
 package src;
-
+// inital commit test - austin
 /**
  *
  * @author alex
  */
 public interface ProductInterface {
- 
+    
     String getProductInfo();
     double getPrice();
     String getName();
